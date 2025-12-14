0e8e2142eb903839efe198b20206d96adccc82e2
mikohenrietta <mkhenike@gmail.com>
2025-12-14 22:14:26 +0200

    Changed routing to master page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -4,12 +4,6 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 export default function LoginPage() {
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-
-  if (isAuthenticated) {
-    return <Navigate to="/vault" replace />;
-  }
-
   return (
     <Box
       sx={{
