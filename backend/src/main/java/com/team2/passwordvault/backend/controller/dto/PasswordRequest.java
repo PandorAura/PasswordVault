e38b0329f18c3591f9fe908f7d1f68b642fd14eb
orcrobert <robyorban@gmail.com>
2026-01-11 20:45:24 +0200

    Fix password strength becoming null and non existent updated_at

--- backend/src/main/java/com/team2/passwordvault/backend/controller/dto/PasswordRequest.java
+++ backend/src/main/java/com/team2/passwordvault/backend/controller/dto/PasswordRequest.java
@@ -1,32 +1,14 @@
 package com.team2.passwordvault.backend.controller.dto;
 
-import lombok.Builder;
-import lombok.Data;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Value;
-import lombok.extern.jackson.Jacksonized;
 
-@Data
-@Value
-@Builder(toBuilder = true)
-@Jacksonized
-public class PasswordRequest {
-
-    @NotBlank(message = "Title is required")
-    String title;
-
-    @NotBlank(message = "Username is required")
-    String username;
-
-    @NotBlank(message = "Encrypted Password is required")
-    String encryptedPassword;
-
-    @NotBlank(message = "Encryption IV is required")
-    String encryptionIv;
-
-    String url;
-
-    String category;
-
-    String notes;
-}
+public record PasswordRequest(
+        @NotBlank(message = "Title is required") String title,
+        @NotBlank(message = "Username is required") String username,
+        @NotBlank(message = "Encrypted Password is required") String encryptedPassword,
+        @NotBlank(message = "Encryption IV is required") String encryptionIv,
+        String url,
+        String category,
+        String notes,
+        String strength
+) {}
--- backend/src/main/java/com/team2/passwordvault/backend/controller/dto/PasswordResponse.java
+++ backend/src/main/java/com/team2/passwordvault/backend/controller/dto/PasswordResponse.java
@@ -1,21 +1,16 @@
 package com.team2.passwordvault.backend.controller.dto;
 
-import lombok.Builder;
-import lombok.Data;
-
 import java.util.UUID;
 
-@Data
-@Builder
-public class PasswordResponse {
-
-    private UUID id;
-    private String title;
-    private String usernameOrEmail;
-    private String websiteUrl;
-    private String encryptedPassword;
-    private String encryptionIv;
-    private String notes;
-    private String category;
-    private String strength;
-}
+public record PasswordResponse(
+        UUID id,
+        String title,
+        String usernameOrEmail,
+        String websiteUrl,
+        String encryptedPassword,
+        String encryptionIv,
+        String notes,
+        String category,
+        String strength,
+        String updatedAt
+) {}
